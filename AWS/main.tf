d1acc3a47cc7afa44d13555c827fdd90cd023126
Igor Makienko <imaki@softserveinc.com>
2019-12-27 15:48:03 +0200

    Added new vars for module webEC2

--- AWS/main.tf
+++ AWS/main.tf
@@ -47,14 +47,18 @@ module "bastion" {
 module "webEC2" {
   source = "./modules/webEC2"
 
-  orchestration    = var.orchestration
-  createdby        = var.createdby
-  environment      = var.environment
-  region           = var.region
-  keyName          = var.keyName
-  vpcId            = module.vpc.vpcId
-  hostedZoneName   = var.hostedZoneName
-  privateSubnet0id = module.vpc.PrivateSubnet0id
-  privateSubnet1id = module.vpc.PrivateSubnet1id
-  httpsListenerArn = module.alb.httpsListenerArn
+  orchestration           = var.orchestration
+  createdby               = var.createdby
+  environment             = var.environment
+  region                  = var.region
+  keyName                 = var.keyName
+  hostedZoneName          = var.hostedZoneName
+  hostedZoneId            = var.hostedZoneId
+  tomcatVersion           = var.tomcatVersion
+  vpcId                   = module.vpc.vpcId
+  privateSubnet0id        = module.vpc.PrivateSubnet0id
+  privateSubnet1id        = module.vpc.PrivateSubnet1id
+  httpsListenerArn        = module.alb.httpsListenerArn
+  albDnsName              = module.alb.albDnsName
+  lbCanonicalHostedZoneId = module.alb.lbCanonicalHostedZoneId
 }
