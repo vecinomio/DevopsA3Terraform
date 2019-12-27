914b2e031f0d81cb47f8d3041d05c4240cd612ce
Igor Makienko <imaki@softserveinc.com>
2019-12-27 15:52:49 +0200

    Modified userdata: Changed tomcat installation method

--- AWS/modules/webEC2/webserversUserData.sh
+++ AWS/modules/webEC2/webserversUserData.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 echo "-------------------------START-----------------------------"
+tomcat_version=${tomcat_version}
+tomcat_ver_maj_okt=$(echo ${tomcat_version} | cut -d '.' -f 1)
 function retryCommand() {
     local ATTEMPTS="$1"
     local SLEEP="$2"
@@ -10,12 +12,15 @@ function retryCommand() {
     done
 }
 echo "-------------INSTALLING TOMCAT AND DEPLOY APP--------------"
-retryCommand 5 10 "yum -y install tomcat tomcat-webapps tomcat-admin-webapps \
-                   tomcat-docs-webapp tomcat-javadoc"
-mv /var/lib/tomcat/webapps/ROOT/ /var/lib/tomcat/webapps/default-ROOT
-wget https://snakes-app.s3.amazonaws.com/ROOT.war
-mv ROOT.war /var/lib/tomcat/webapps/
-systemctl start tomcat
-systemctl enable tomcat
-sleep 10s
-curl -sS http://localhost:8080 | grep "Does it have snakes?"
+cd /tmp
+retryCommand 5 10 "yum install java-1.8.0-openjdk -y"
+retryCommand 5 10 "aws s3 cp 's3://snakes-app/ROOT.war' ."
+wget http://apache.volia.net/tomcat/tomcat-$tomcat_ver_maj_okt/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz
+tar xzf apache-tomcat-${tomcat_version}.tar.gz
+mv apache-tomcat-${tomcat_version}/ /usr/local/tomcat/ && rm apache-tomcat-${tomcat_version}.tar.gz
+mv /usr/local/tomcat/webapps/ROOT/ /usr/local/tomcat/webapps/default-ROOT
+mv ROOT.war /usr/local/tomcat/webapps/
+/usr/local/tomcat/bin/catalina.sh start
+
+retryCommand 5 10 "curl -sS http://localhost:8080 | grep 'Does it have snakes?'"
+echo "-------------------------FINISH----------------------------"
